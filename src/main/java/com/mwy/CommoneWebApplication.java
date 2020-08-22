d5e85cb055d5f7b3051449702aeba3b10940a61d
mouwenyao <mouwenyao@didiglobal.com>
2020-08-22 19:14:49 +0800

    init

---
+++ src/main/java/com/mwy/CommoneWebApplication.java
@@ -0,0 +1,13 @@
+package com.mwy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommoneWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommoneWebApplication.class, args);
+    }
+
+}
